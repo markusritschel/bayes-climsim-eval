7d1a00615aa94815af59889168a961a12d4cbae1
markusritschel <git@markusritschel.de>
2024-04-29 18:01:35 +0200

    Add BayesEval class

---
+++ src/bayes_eval.py
@@ -0,0 +1,16 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-04-28
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+class BayesEval:
+    """A class to perform statistical evaluation of different climate scenarios based on Bayesian methods."""
+    def __init__(self):
+        pass
