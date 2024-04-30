7a7a6b525ed78312e117dbe912a190a969efebd2
markusritschel <git@markusritschel.de>
2024-04-30 15:25:48 +0200

    Add method for adding datasets

--- src/bayes_eval.py
+++ src/bayes_eval.py
@@ -13,4 +13,11 @@ log = logging.getLogger(__name__)
 class BayesEval:
     """A class to perform statistical evaluation of different climate scenarios based on Bayesian methods."""
     def __init__(self):
+        self._original_data = {}
+
+    def add(self, *args, **kwargs):
+        if args and isinstance(args[0], dict):
+            self._original_data.update(args[0])
+        else:
+            self._original_data.update(kwargs)
         pass
