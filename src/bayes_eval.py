8be0ae76befe5a4a1eea48060ad3143012b07498
markusritschel <git@markusritschel.de>
2024-04-30 15:29:06 +0200

    Add method to project data

--- src/bayes_eval.py
+++ src/bayes_eval.py
@@ -5,6 +5,8 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
 import logging
+from my_code_base.linalg import inv
+import numpy as np
 
 
 log = logging.getLogger(__name__)
@@ -14,10 +16,17 @@ class BayesEval:
     """A class to perform statistical evaluation of different climate scenarios based on Bayesian methods."""
     def __init__(self):
         self._original_data = {}
+        self._projected_data = {}
 
     def add(self, *args, **kwargs):
         if args and isinstance(args[0], dict):
             self._original_data.update(args[0])
         else:
             self._original_data.update(kwargs)
-        pass
+        self._projected_data = {}
+
+    def project_onto(self, guess_matrix):
+        """Project all datasets onto a matrix of guess vectors. This is the step of data reduction."""
+        G = guess_matrix
+        P = inv(G.T.dot(G)).dot(G.T)
+        self._projected_data = {k:P.dot(x) for (k,x) in self._original_data.items()}
